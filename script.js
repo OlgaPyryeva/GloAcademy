e389dd637fdc504f86469e23582bfa7983b9da75
Olga Pyryeva <vashkevicholga@gmail.com>
2022-01-28 19:59:43 +0300

    Assigned variables

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let title = "";
-let screens = "";
-let screenPrice = "";
-let rollback = "";
-let fullPrice = "";
-let adaptive = "";
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+title = "My first project";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 42;
+rollback = 56;
+fullPrice = 2500;
+adaptive = true;
